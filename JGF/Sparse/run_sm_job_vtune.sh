94157bde9bd084ee248eeb6de872bfae45a92dc7
NunoOliv <nuno.oliv.94@gmail.com>
2016-12-06 19:57:07 +0000

    Updaded Job Scripts

---
+++ JGF/Sparse/run_sm_job_vtune.sh
@@ -0,0 +1,39 @@
+# PBS -N 
+#PBS -l walltime=00:05:00
+#PBS -q mei
+
+# PBS -m abe
+# PBS -M nuno.oliv.94@gmail.com
+
+#PBS -l nodes=1:r662:ppn=4
+
+# Directories
+Project_Folder="/home/a67649/tese/benchmarks/ScholarShip_public/JGF/Sparse/JGF_original"
+Vtune_output_dir="/home/a67649/tese/benchmarks/ScholarShip_public/JGF/Sparse/JGF_original/Vtune_out/sm"
+# Inicializar Java (Não Necessario)
+
+# Inicializar VTune
+source /share/apps/intel/vtune_amplifier_xe_2017/amplxe-vars.sh
+source /share/apps/intel/vtune_amplifier_xe_2017/sep_vars.sh
+
+# Ir Para Pasta
+cd $Project_Folder
+
+# Compilar
+mkdir -p classes
+# seq
+javac -g -d ./classes ./sm/*.java
+# Correr com VTune
+# Criar pasta
+mkdir -p "/home/a67649/tese/benchmarks/ScholarShip_public/JGF/Sparse/JGF_original/Vtune_out/sm"
+cd $Vtune_output_dir
+
+echo "Collecting HotSpots"
+(>&2 echo "Collecting hotspots")
+amplxe-cl -collect hotspots -- java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/classes/ sm.Sm_jgf -size 5 4
+echo "Collecting Memory-access"
+(>&2 echo "Collecting Memory_Acess")
+amplxe-cl -collect memory-access -- java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/classes/ sm.Sm_jgf -size 5 4
+echo "Collecting Disk-io"
+(>&2 echo "Collecting Disk-io")
+amplxe-cl -collect disk-io -- java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/classes/ sm.Sm_jgf -size 5 4
