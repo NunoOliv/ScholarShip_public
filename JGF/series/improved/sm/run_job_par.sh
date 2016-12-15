9e91be1100c45bb751351493e54ca97aba82b605
NunoOliv <nuno.oliv.94@gmail.com>
2016-12-15 23:16:07 +0000

    more changes

---
+++ JGF/series/improved/sm/run_job_par.sh
@@ -0,0 +1,2 @@
+qsub -v JavaV="j8",Test="ge" run_sm_job_vtune.sh
+qsub -v JavaV="j9",Test="ge" run_sm_job_vtune.sh
--- JGF/series/improved/sm/run_sm_job_vtune.sh
+++ JGF/series/improved/sm/run_sm_job_vtune.sh
@@ -1,5 +1,5 @@
 # PBS -N 
-#PBS -l walltime=00:15:00
+#PBS -l walltime=00:05:00
 #PBS -q mei
 
 # PBS -m abe
@@ -7,15 +7,13 @@
 
 #PBS -l nodes=1:r662:ppn=4
 
-
 # Directories
-if [ $JavaV="j9" ]; then
+if [ $JavaV == "j9" ]; then
     Java_dir="/home/a67649/apps/jdk9/bin/"
-elif [ $JavaV="j8" ]; then
+elif [ $JavaV == "j8" ]; then
     Java_dir="/share/apps/java/jdk1.8.0_20/bin/"
     else exit 1
 fi
-
 
 Project_Folder="/home/a67649/tese/benchmarks/ScholarShip_public/JGF/series/improved"
 Vtune_output_dir="/home/a67649/tese/benchmarks/ScholarShip_public/JGF/series/improved/Vtune_out/smSeries_$JavaV"
@@ -38,16 +36,22 @@ ${Java_dir}/javac -g -d ./$Classes_dir ./sm/*.java
 # Criar pasta
 mkdir -p "/home/a67649/tese/benchmarks/ScholarShip_public/JGF/series/improved/Vtune_out/smSeries_$JavaV"
 cd $Vtune_output_dir
-
 (>&2 echo "${Project_Folder}/${Classes_dir}/")
-echo "Collecting HotSpots"
-(>&2 echo "Collecting hotspots")
-amplxe-cl -collect hotspots -- ${Java_dir}/java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/${Classes_dir}/ sm_improved.JGFSeriesBenchSizeA -size 5 4
-echo "Collecting Memory-access"
-(>&2 echo "Collecting Memory_Acess")
-amplxe-cl -collect memory-access -- ${Java_dir}/java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/${Classes_dir}/ sm_improved.JGFSeriesBenchSizeA -size 5 4
-(>&2 echo "Collecting advanced-hotspots")
-amplxe-cl -collect advanced-hotspots -- ${Java_dir}/java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/${Classes_dir}/ sm_improved.JGFSeriesBenchSizeA -size 5 4
-echo "Collecting general-exploration"
-(>&2 echo "Collecting general-exploration")
-amplxe-cl -collect general-exploration -- ${Java_dir}/java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/${Classes_dir}/ sm_improved.JGFSeriesBenchSizeA -size 5 4
+
+if [ $Test == "hs" ]; then
+	echo "Collecting HotSpots"
+	(>&2 echo "Collecting hotspots")
+	amplxe-cl -collect hotspots -- ${Java_dir}/java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/${Classes_dir}/ sm_improved.JGFSeriesBenchSizeA -size 5 4
+elif [ $Test == "ma" ]; then
+	echo "Collecting Memory-access"
+	(>&2 echo "Collecting Memory_Acess")
+	amplxe-cl -collect memory-access -- ${Java_dir}/java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/${Classes_dir}/ sm_improved.JGFSeriesBenchSizeA -size 5 4
+elif [ $Test == "ah" ]; then
+	echo "Collecting advanced-hotspots"
+	(>&2 echo "Collecting advanced-hotspots")
+	amplxe-cl -collect advanced-hotspots -- ${Java_dir}/java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/${Classes_dir}/ sm_improved.JGFSeriesBenchSizeA -size 5 4
+elif [ $Test == "ge" ]; then
+	echo "Collecting general-exploration"
+	(>&2 echo "Collecting general-exploration")
+	amplxe-cl -collect general-exploration -- ${Java_dir}/java -Xcomp -Djava.library.path=native_lib/ia32 -cp ${Project_Folder}/${Classes_dir}/ sm_improved.JGFSeriesBenchSizeA -size 5 4
+fi
